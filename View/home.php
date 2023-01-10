4234145cfeafeeccac61c690a4791ebe1b9af564
Salim <tizi_salim@yahoo.fr>
2023-01-10 19:47:48 +0100

    homepage is now the same page when you log or not but nav is not the same : log out or login

--- View/home.php
+++ View/home.php
@@ -1,8 +1,28 @@
 <body>
+    <h1>Bienvenue
+        <?php
+        if (isset($result)) {
+            echo $user['firstName'];
+        }
+        ?>
+    </h1>
+    <div class="user-widget">
+        <?php if (isset($_SESSION['user']) && $_SESSION['user'] !== null) { ?>
+        <a href="index.php?ctrl=user&action=home">Home</a>
+        <a href="index.php?ctrl=user&action=logOut">Se déconnecter</a>
+        <?php
+        } else { ?>
 
-    <header>
-        <h1>Bienvenue</h1>
-    </header>
-    <a href="index.php?ctrl=user&action=login">Login</a>
-    <a href="index.php?ctrl=user&action=create">Create account</a>
+        <a href="index.php?ctrl=user&action=login">Login</a>
+        <a href="index.php?ctrl=user&action=create">Create account</a>
+        <?php }
+        ?>
+    </div>
+    <p>
+        <?php
+        if (isset($result)) {
+            echo $info;
+        }
+        ?>
+    </p>
 </body>
