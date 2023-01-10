a3999aaa63b6bcdad8dd2b0b290f58477376978b
Salim <tizi_salim@yahoo.fr>
2023-01-10 19:42:58 +0100

    add home in nav

--- View/signup.php
+++ View/signup.php
@@ -2,6 +2,7 @@
     <header>
         <h1>Création/Modification d'un User</h1>
     </header>
+    <a href="index.php?ctrl=user&action=home">Home</a>
     <hr />
     <section id="main-section">
         <form action="index.php?ctrl=user&action=doCreate" method="POST">
